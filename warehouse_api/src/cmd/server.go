77e05abf3dcbbd917a9c9c69f0a1278888c4596c
klan300 <nathun.thun@hotmail.com>
2020-07-23 15:43:26 +0700

    fix all symbol

--- warehouse_api/src/cmd/server.go
+++ warehouse_api/src/cmd/server.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
-    "ava.fund/alpha/Post-Covid/warehouse_api/src/internal/handlers"
-    "ava.fund/alpha/Post-Covid/warehouse_api/src/internal/utils"
+	"ava.fund/alpha/Post-Covid/warehouse_api/src/internal/handlers"
+	"ava.fund/alpha/Post-Covid/warehouse_api/src/internal/utils"
 )
 
 func main() {
 
-    if len(os.Args) != 2 {
-        fmt.Printf("Usage: %s <config>\n", os.Args[0])
-        os.Exit(0)
-    }
-    utils.LoadConfig(os.Args[1])
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s <config>\n", os.Args[0])
+		os.Exit(0)
+	}
+	utils.LoadConfig(os.Args[1])
+	utils.Debug("[server.go] Begin")
 
-    e := echo.New()
-    e.Use(middleware.Recover())
-    e.Use(middleware.CORS())
-    e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-        Format : "${time_rfc3339}: FROM: ${remote_ip} ${method}: ${uri} RETURN: ${status}\n",
-        Skipper: middleware.DefaultSkipper,
-        Output : utils.Config.Logging.Outputs(),
-    }))
-    e.POST("/token", handlers.Token)
-    
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format:  "${time_rfc3339}: FROM: ${remote_ip} ${method}: ${uri} RETURN: ${status}\n",
+		Skipper: middleware.DefaultSkipper,
+		Output:  utils.Config.Logging.Outputs(),
+	}))
+	e.POST("/token", handlers.Token)
 
-    api := e.Group("/api")
-    api.Use(middleware.JWT([]byte(utils.Config.Authen.Secret)))
-    api.GET("/financials/:statement/:frequency", handlers.Financials) 
-    api.GET("/profile", handlers.Profile)
-    api.GET("/candle", handlers.Candle)
-    api.GET("/symbols", handlers.Symbols)
-    api.GET("/search", handlers.Search)
+	api := e.Group("/api")
+	api.Use(middleware.JWT([]byte(utils.Config.Authen.Secret)))
+	api.GET("/financials/:statement/:frequency", handlers.Financials)
+	api.GET("/profile", handlers.Profile)
+	api.GET("/candle", handlers.Candle)
+	api.GET("/symbols", handlers.Symbols)
+	api.GET("/search", handlers.Search)
 
-    e.Logger.Fatal(e.Start(utils.Config.Target.Host))
+	e.Logger.Fatal(e.Start(utils.Config.Target.Host))
+	utils.Debug("[server.go] End")
 }
-
--- warehouse_api/src/internal/utils/database.go
+++ warehouse_api/src/internal/utils/database.go
@@ -1,41 +1,43 @@
 package utils
 
 import (
-    "context"
-    "time"
+	"context"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Database() (*mongo.Database, context.Context){
-   
-    Debug("[database.go] Begin")
+func Database() (*mongo.Database, context.Context) {
 
-    Debug("[database.go] Create a database client")
+	Debug("[database.go] Begin")
+
+	Debug("[database.go] Create a database client")
+	// client, err := mongo.NewClient(options.
+	// 	Client().
+	// 	ApplyURI(Config.Source.Host).
+	// 	SetAuth(options.Credential{
+	// 		Username: Config.Source.Username,
+	// 		Password: Config.Source.Password,
+	// 	}),
+    // )
+    
     client, err := mongo.NewClient(options.
-        Client().
-        ApplyURI(Config.Source.Host).
-        SetAuth(
-        options.Credential{
-         Username: Config.Source.Username, 
-         Password: Config.Source.Password,
-        }))
+		Client().
+		ApplyURI("mongodb://localhost:27017"),
+	)
 
-    
-    if err != nil {
-        Error("[database.go] %v", err)
-    }
+	if err != nil {
+		Error("[database.go] %v", err)
+	}
 
+	Debug("[database.go] Create a connection to %s", Config.Source.Host)
+	ctx, _ := context.WithTimeout(context.Background(), 24*time.Hour)
+	err = client.Connect(ctx)
+	if err != nil {
+		Error("[database.go] %v", err)
+	}
 
-    Debug("[database.go] Create a connection to %s", Config.Source.Host)
-    ctx, _ := context.WithTimeout(context.Background(), 24 * time.Hour)
-    err = client.Connect(ctx)
-    if err != nil {
-        Error("[database.go] %v", err)
-    }
-
-    database := client.Database(Config.Source.Database)
-    
-    return database, ctx
+	database := client.Database(Config.Source.Database)
+	return database, ctx
 }
