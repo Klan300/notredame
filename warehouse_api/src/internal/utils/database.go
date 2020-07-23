8ce5a38af70fef0462612ee6f93c780ee3507114
klan300 <nathun.thun@hotmail.com>
2020-07-23 16:18:15 +0700

    fix database

--- warehouse_api/src/internal/utils/database.go
+++ warehouse_api/src/internal/utils/database.go
@@ -13,19 +13,14 @@ func Database() (*mongo.Database, context.Context) {
 	Debug("[database.go] Begin")
 
 	Debug("[database.go] Create a database client")
-	// client, err := mongo.NewClient(options.
-	// 	Client().
-	// 	ApplyURI(Config.Source.Host).
-	// 	SetAuth(options.Credential{
-	// 		Username: Config.Source.Username,
-	// 		Password: Config.Source.Password,
-	// 	}),
-    // )
-    
-    client, err := mongo.NewClient(options.
+	client, err := mongo.NewClient(options.
 		Client().
-		ApplyURI("mongodb://localhost:27017"),
-	)
+		ApplyURI(Config.Source.Host).
+		SetAuth(options.Credential{
+			Username: Config.Source.Username,
+			Password: Config.Source.Password,
+		}),
+    )
 
 	if err != nil {
 		Error("[database.go] %v", err)
