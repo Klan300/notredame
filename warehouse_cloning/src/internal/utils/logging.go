0500a472d4886694668ae8d753c32c162a6a44f2
klan300 <nathun.thun@hotmail.com>
2020-07-29 18:07:13 +0700

    fix

--- warehouse_cloning/src/internal/utils/logging.go
+++ warehouse_cloning/src/internal/utils/logging.go
@@ -36,17 +36,11 @@ func LoadLoggingConfig() {
 
     if Config.Logging.Dirname != "" {
 
-        rootDirname, err := filepath.Abs(filepath.Dir(os.Args[0]))
-        if err != nil {
-            log.Panicf("[logging.go] %v\n", err)
-        }
-
-        logDirname     := filepath.Join(rootDirname, Config.Logging.Dirname)
         var dirPermissions os.FileMode = 0755
-        os.MkdirAll(logDirname, dirPermissions)
+        os.MkdirAll(Config.Logging.Dirname, dirPermissions)
 
         fileName       := fmt.Sprintf("%s.log", time.Now().Format("2006-01-02"))
-        filePath       := filepath.Join(logDirname, fileName)
+        filePath       := filepath.Join(Config.Logging.Dirname, fileName)
         fileOperations := os.O_CREATE | os.O_APPEND | os.O_WRONLY
 
         var filePermissions os.FileMode = 0644
